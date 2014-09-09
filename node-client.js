ee534b5f59e5aa0b19b5583c28fb51bb616f19fa
Christian Vogt <mail@christianvogt.de>
2014-09-09 09:06:16 +0200

    Added REST interface; Added message proxy
    * Peers now proxy all messages to the host
    * If a log handler is registered, these messages can be passed to a
    mediator instance
    * Added REST interface functionality (add/abort peer, status)

--- node-client.js
+++ node-client.js
@@ -6,9 +6,13 @@ var BopPingProto = require('./js/protocols.ping.js');
 var bootstrapNode = process.argv[2];
 
 var bopclient = new BOPlishClient(bootstrapNode, function(msg) {
-    process.send({error: null, id: bopclient.id});
+    process.send({type: 'system', error: null, id: bopclient.id});
     // Register protocols
 	new BopPingProto(bopclient);
 }, function(err) {
     process.send({error: err, id: null});
+});
+
+bopclient.setMonitorCallback(function(from, payload) {
+	process.send({type: 'boplishMessage', from: from, payload: payload});
 });
--- run.js
+++ run.js
@@ -6,6 +6,7 @@
 var logger = require('winston');
 var program = require('commander');
 var fork = require('child_process').fork;
+var restify = require('restify');
 
 /** 
  * Module configuration
@@ -15,35 +16,122 @@ logger.add(logger.transports.Console, {'timestamp': true, 'colorize': true});
 program
     .version('0.0.2')
     .option('-b, --bootstrap <URI>', 'Bootstrap Host information', String, 'wss://127.0.0.1/')
-    .option('-c, --count <count>', 'Number of clients to spawn', Number, 1)
+    .option('-l, --listen <IP:port>', 'Listener IP', String, '127.0.0.1:8080')
     .parse(process.argv);
 
 var bootstrapNode = program.bootstrap;
+var listen = program.listen;
+var listenIP = listen.substring(0,listen.indexOf(':'));
+var listenPort = listen.substring(listen.indexOf(':') + 1);
+var server = restify.createServer();
 
-function spawnBopClients(quantity, onsuccess, onerror) {
-    if (quantity <= 0) {
-        if (typeof(onsuccess) === 'function') {
-            onsuccess();
+var peers = {};
+
+var Mediator = function() {};
+Mediator.prototype = {
+    websocket: null,
+    send: function(msg) {
+        try {
+            this.websocket.send(msg);
+        } catch(e) {
+            // WS not ready yet
         }
-        return;
     }
+}
+var mediator = new Mediator();
 
+server.post('/peer', rest_startPeer);
+server.del('/peer/:id', rest_abortPeer);
+server.get('/peer/:id', rest_getStatusOfPeer);
+server.get('/listAllIds', rest_listAllIds);
+server.get('/killAll', rest_killAll);
+server.get('/getLogHandler', rest_registerLogHandler);
+
+function killPeer(peerId) {
+    if (typeof(peers[peerId]) !== 'undefined') {
+        var peer = peers[peerId];
+        peer.process.kill();no
+        delete peers[peerId];
+        return peer;
+    } else {
+        return Error('invalid id: ' + peerId);
+    }
+}
+
+function rest_startPeer(req, res, next) {
     var client = fork('./node-client.js', [bootstrapNode]);
     client.on('message', function(msg) {
-    	if (msg.error) {
-    		onerror(msg.error);
-    	} else {
-    		logger.info(msg.id, 'spawned');
-                spawnBopClients(--quantity, onsuccess, onerror);
-    	}
+        if (!msg || msg.error) {
+            res.send(new Error(msg.error));
+            next();
+        } else if (msg.type === 'system'){
+            logger.info(msg.id, 'spawned');
+            peers[msg.id] = {};
+            peers[msg.id].id = msg.id;
+            peers[msg.id].process = client;
+            peers[msg.id].started = new Date();
+            peers[msg.id].bootstrapNode = bootstrapNode;
+            res.send({id: msg.id});
+            next();
+        } else if (msg.type === 'boplishMessage') {
+            mediator.send(msg);
+        }
     });
 }
 
-logger.info('Spawning ' + program.count + ' BOPlish client(s) using ' + bootstrapNode + ' as bootstrap server');
-spawnBopClients(
-    program.count,
-    function(msg){
-        logger.info('All BOPlish clients spawned');
-    }, function(err){
-        logger.error('Could not spawn all BOPlish clients');
+function rest_abortPeer(req, res, next) {
+    var peer = killPeer(req.params.id);
+    var response = {
+        id: peer.id,
+        status: 'killed'
+    }
+    res.send(response);
+    next();
+}
+
+function rest_getStatusOfPeer(req, res, next) {
+    if (typeof(peers[req.params.id]) === 'undefined') {
+        res.send('invalid id ' + req.params.id);
+        return next();
+    }
+    var peer = peers[req.params.id];
+    var response = {
+        id: req.params.id,
+        started: peer.started,
+        boostrapNode: peer.bootstrapNode
+    }
+    res.send(response);
+    next();
+}
+
+function rest_listAllIds(req, res, next) {
+    var keys = [];
+    for(var k in peers) {
+        keys.push(k);
+    }
+    res.send(keys);
+    next();
+}
+
+function rest_killAll(req, res, next) {
+    for(var k in peers) {
+        killPeer(k);
+    }
+    res.send({status: 'All Peers killed'});
+    next();
+}
+
+function rest_registerLogHandler(req, res, next) {
+    if (!res.claimUpgrade) {
+        next(new Error('Connection Must Upgrade For WebSockets'));
+        return;
+    }
+
+    var upgrade = res.claimUpgrade();
+    mediator.websocket = ws.accept(req, upgrade.socket, upgrade.head);
+    next(false);
+}
+
+server.listen(listenPort, function() {
+    logger.info('BOPlish host listening on ' + listenIP + ':' + listenPort);
 });
